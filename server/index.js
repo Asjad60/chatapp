0da36f8732447247da71ed0a2d34bb2ea66fb7b8
Asajd60 <mrajgamer038@gmail.com>
2024-07-25 21:59:30 +0530

    updated index file

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
@@ -39,7 +39,7 @@ cloudinaryConnect();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
