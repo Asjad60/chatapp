be56739e395e6a3ca38d54b18e32a11695c23303
Asjad60 <mrajgamer038@gmail.com>
2025-06-24 22:12:14 +0530

    update chat header

--- src/components/core/Chat/ChatProfileHeader.jsx
+++ src/components/core/Chat/ChatProfileHeader.jsx
@@ -1,9 +1,10 @@
 import React, { forwardRef, useEffect, useRef, useState } from "react";
 import Button from "../../Button";
 import { BsThreeDotsVertical } from "react-icons/bs";
-import { AnimatePresence, motion, stagger } from "framer-motion";
-import { useParams, useSearchParams } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
+import { Link, useParams, useSearchParams } from "react-router-dom";
 import { fetchGroupDetails } from "../../../services/operations/groupAPI";
+import { FaLongArrowAltLeft } from "react-icons/fa";
 
 const ChatProfileHeader = forwardRef(
   ({ setBgImageIndex, bgArray, userId }, containerRef) => {
@@ -12,9 +13,13 @@ const ChatProfileHeader = forwardRef(
     const imageUrl = searchParams.get("imageUrl");
     const isUsernameParam = searchParams.has("username");
     const { id: groupId } = useParams();
+
     const [groupDetails, setGroupDetails] = useState(null);
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    const [containerWidth, setContainerWidth] = useState(0);
     const [isOpen, setIsOpen] = useState(false);
     const [imagePos, setImagePos] = useState(null);
+
     const imageRef = useRef();
 
     const containerVariants = {
@@ -66,6 +71,24 @@ const ChatProfileHeader = forwardRef(
       }
     }, [groupId]);
 
+    useEffect(() => {
+      const handleResize = () => {
+        setScreenWidth(window.innerWidth);
+        if (containerRef.current) {
+          setContainerWidth(containerRef.current.offsetWidth);
+        }
+      };
+
+      handleResize(); // initial run
+
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    const isMobile = screenWidth <= 480;
+    const animatedSize = isMobile ? 200 : 300;
+    const left = containerWidth / 2 - animatedSize / 2;
+
     // console.log("groupDetails: ", groupDetails);
 
     return (
@@ -74,11 +97,23 @@ const ChatProfileHeader = forwardRef(
           className="flex items-center gap-3 p-4 text-white cursor-pointer"
           onClick={openZoom}
         >
-          <img
-            ref={imageRef}
-            src={isUsernameParam ? imageUrl : groupDetails?.groupProfile}
-            className="w-10 h-10 rounded-full object-cover "
-          />
+          <Link to="/" className="text-slate-300">
+            <FaLongArrowAltLeft size={25} />
+          </Link>
+          {isUsernameParam || groupDetails?.groupProfile ? (
+            <img
+              ref={imageRef}
+              src={isUsernameParam ? imageUrl : groupDetails?.groupProfile}
+              className="w-10 h-10 rounded-full object-cover "
+            />
+          ) : (
+            <div
+              ref={imageRef}
+              className="capitalize p-2 rounded-full bg-[#255487] w-[40px] h-[40px] flex justify-center items-center"
+            >
+              {groupDetails?.groupName[0]}
+            </div>
+          )}
           <h2>{isUsernameParam ? username : groupDetails?.groupName}</h2>
         </div>
 
@@ -86,43 +121,75 @@ const ChatProfileHeader = forwardRef(
         <AnimatePresence>
           {isOpen && imagePos && (
             <motion.div
-              className="absolute inset-0 bg-black text-slate-100 bg-opacity-60 backdrop-blur-sm z-50 flex flex-col"
+              className="absolute inset-0 bg-black text-slate-100 bg-opacity-60 backdrop-blur-sm z-50 flex flex-col overflow-y-auto"
               onClick={closeZoom}
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
             >
-              <motion.img
-                src={isUsernameParam ? imageUrl : groupDetails?.groupProfile}
-                initial={{
-                  position: "absolute",
-                  top: imagePos.y,
-                  left: imagePos.x,
-                  width: imagePos.width,
-                  height: imagePos.height,
-                  borderRadius: "9999px",
-                }}
-                animate={{
-                  //   top: imagePos.containerHeight / 2 - 150, // center vertically in parent
-                  left: imagePos.containerWidth / 2 - 150, // center horizontally in parent
-                  width: 300,
-                  height: 300,
-                  borderRadius: "9999px",
-                }}
-                exit={{
-                  top: imagePos.y,
-                  left: imagePos.x,
-                  width: imagePos.width,
-                  height: imagePos.height,
-                  borderRadius: "9999px",
-                }}
-                transition={{ duration: 0.4 }}
-                onClick={(e) => e.stopPropagation()}
-                className="object-cover"
-              />
+              {isUsernameParam || groupDetails?.groupProfile ? (
+                <motion.img
+                  src={isUsernameParam ? imageUrl : groupDetails?.groupProfile}
+                  initial={{
+                    position: "absolute",
+                    top: imagePos.y,
+                    left: imagePos.x,
+                    width: imagePos.width,
+                    height: imagePos.height,
+                    borderRadius: "9999px",
+                  }}
+                  animate={{
+                    //   top: imagePos.containerHeight / 2 - 150, // center vertically in parent
+                    left: left, // center horizontally in parent
+                    width: animatedSize,
+                    height: animatedSize,
+                    borderRadius: "9999px",
+                  }}
+                  exit={{
+                    top: imagePos.y,
+                    left: imagePos.x,
+                    width: imagePos.width,
+                    height: imagePos.height,
+                    borderRadius: "9999px",
+                  }}
+                  transition={{ duration: 0.4 }}
+                  onClick={(e) => e.stopPropagation()}
+                  className="object-cover"
+                />
+              ) : (
+                <motion.div
+                  initial={{
+                    position: "absolute",
+                    top: imagePos.y,
+                    left: imagePos.x,
+                    width: imagePos.width,
+                    height: imagePos.height,
+                    borderRadius: "9999px",
+                  }}
+                  animate={{
+                    //   top: imagePos.containerHeight / 2 - 150, // center vertically in parent
+                    left: left, // center horizontally in parent
+                    width: animatedSize,
+                    height: animatedSize,
+                    borderRadius: "9999px",
+                  }}
+                  exit={{
+                    top: imagePos.y,
+                    left: imagePos.x,
+                    width: imagePos.width,
+                    height: imagePos.height,
+                    borderRadius: "9999px",
+                  }}
+                  transition={{ duration: 0.4 }}
+                  onClick={(e) => e.stopPropagation()}
+                  className="capitalize text-6xl p-2 rounded-full bg-[#255487] w-[40px] h-[40px] flex justify-center items-center"
+                >
+                  {groupDetails?.groupName[0]}
+                </motion.div>
+              )}
 
               {groupDetails && !isUsernameParam && (
-                <div className="mt-[22rem]">
+                <div className="mt-[16rem] min-[480px]:mt-[22rem]">
                   <div>
                     <h3 className={"text-2xl text-center  font-bold"}>
                       {groupDetails?.groupName}
